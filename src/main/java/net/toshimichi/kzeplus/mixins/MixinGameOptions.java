7a9bb20e06310e78a0bde3d2125deede77ab7760
Toshimichi0915 <main@toshimichi.net>
2023-08-22 20:45:16 +0900

    refactor: accessorを削除してhot reloadが動くよう修正

--- src/main/java/net/toshimichi/kzeplus/mixins/MixinGameOptions.java
+++ src/main/java/net/toshimichi/kzeplus/mixins/MixinGameOptions.java
@@ -4,6 +4,7 @@ import net.minecraft.client.option.GameOptions;
 import net.minecraft.client.option.KeyBinding;
 import net.toshimichi.kzeplus.KzePlus;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -11,20 +12,19 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(GameOptions.class)
 public class MixinGameOptions {
 
+    @Shadow public KeyBinding[] allKeys;
+
     @Inject(method = "load", at = @At("HEAD"))
     private void registerKeys(CallbackInfo ci) {
-        // set up key bindings
-        AccessorGameOptions gameOptions = (AccessorGameOptions) this;
 
         // add new keys
-        KeyBinding[] allKeys = gameOptions.getAllKeys();
         KeyBinding[] toAdd = new KeyBinding[]{KzePlus.VISIBILITY_TOGGLE_KEY};
         KeyBinding[] newAllKeys = new KeyBinding[allKeys.length + toAdd.length];
         System.arraycopy(allKeys, 0, newAllKeys, 0, allKeys.length);
         System.arraycopy(toAdd, 0, newAllKeys, allKeys.length, toAdd.length);
-        gameOptions.setAllKeys(newAllKeys);
+        allKeys = newAllKeys;
 
         // add new categories
-        AccessorKeybinding.getCategoryOrderMap().put(KzePlus.KZE_CATEGORY, 10000);
+        KeyBinding.CATEGORY_ORDER_MAP.put(KzePlus.KZE_CATEGORY, 10000);
     }
 }
